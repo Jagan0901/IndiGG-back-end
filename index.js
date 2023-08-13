0fdb367413448699e670742f8894ba48d8d98078
Jagan0901 <Jaganath0901@gmail.com>
2023-08-13 16:46:44 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+import { usersRouter } from "./routes/usersRouter.js";
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+//interceptor || converting body to json
+app.use(express.json());
+
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
+
+async function createConnection() {
+  const client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log("Mongo is connected");
+  return client;
+}
+
+export const client = await createConnection();
+
+app.use("/users", usersRouter);
+// app.use("/tournaments", tournamentRouter);
+
+app.get("/", (req, res) => {
+  res.send(`Hi There !!!`);
+});
+
+app.listen(PORT, () => console.log("Server started on the PORT", PORT));
